151fe595439c023f212fa4aa05266ed241bb28e7
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-24 15:21:42 +0600

    home-slider

--- assets/js/home-slider/homeslider.js
+++ assets/js/home-slider/homeslider.js
@@ -1,41 +1,17 @@
-const popularSlider = new Swiper('.popularSlider', {
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: true,
-  },
-  loop: true,
-  speed: 750,
-  parallax: true,
-  spaceBetween: 60,
-  pagination: false,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 2,
-    },
-    576: {
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 4,
-    },
-    992: {
-      slidesPerView: 3,
-    },
-    1100: {
-      slidesPerView: 4,
-    },
-    1200: {
-      slidesPerView: 5,
-    },
-    1600: {
-      slidesPerView: 6,
-    },
-    1800: {
-      slidesPerView: 7,
-    },
-  },
-})
+$(document).ready(function () {
+  $('.heroslider').slick({
+    autoplay: true,
+    autoplaySpeed: 6500,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    dots: true,
+    cssEase: 'ease-out',
+    waitForAnimate: false,
+    fade: true,
+    draggable: false,
+    prevArrow: false,
+    nextArrow: false,
+  });
+});
