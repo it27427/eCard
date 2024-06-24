400275ed603c88d0705087cc0a44079c64a736fb
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-24 15:17:23 +0600

    first commit

---
+++ assets/js/back-top/backtotop.js
@@ -0,0 +1,18 @@
+up({
+  // bottom position
+  bottom: "3.5rem",
+  // right position
+  right: "1.5rem",
+  // width
+  width: "3rem",
+  // height
+  height: "3rem",
+  // background color
+  bg: "hsl(167, 38%, 53%)",
+  // custom icon
+  src: "assets/cursor/chevron-up-solid.svg",
+  // distance from the top to show the back to top button
+  whenShow: 400,
+  // circular button?
+  circle: true,
+});
