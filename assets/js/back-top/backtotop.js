f633f226827243e71f9dfc96db1fb66b5cc32af2
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-27 13:33:37 +0600

    header and footer completed

--- assets/js/back-top/backtotop.js
+++ assets/js/back-top/backtotop.js
@@ -1,16 +1,16 @@
 up({
   // bottom position
-  bottom: "3.5rem",
+  bottom: '3.5rem',
   // right position
-  right: "1.5rem",
+  right: '1.5rem',
   // width
-  width: "3rem",
+  width: '3rem',
   // height
-  height: "3rem",
+  height: '3rem',
   // background color
-  bg: "hsl(167, 38%, 53%)",
+  bg: 'hsl(219, 54%, 37%)',
   // custom icon
-  src: "assets/cursor/chevron-up-solid.svg",
+  src: 'assets/cursor/chevron-up-solid.svg',
   // distance from the top to show the back to top button
   whenShow: 400,
   // circular button?
